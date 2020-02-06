4d4edb18050aed132b28e33f39dc8223347b00d1
Olga Ioffe <yoffic@yahoo.com>
2020-02-06 23:59:08 +0300

    Update: index.js

--- src/index.js
+++ src/index.js
@@ -1,44 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import _ from 'lodash';
 import axios from 'axios';
-
-import * as yup from 'yup';
-import { watch } from 'melanke-watchjs';
+import render from './renders';
 import parseRSS from './parser';
-
-const errorMessage = {
-  url: 'Введите действующий адрес сайта',
-  double: 'Данный сайт уже добавлен в ленту. Введите новый адрес.',
-};
-
-const checkoutUrl = yup.object().shape({
-  urlAddress: yup.string().url().required(),
-});
-
-const isValidUrl = (text) => {
-  const textObject = { urlAddress: text };
-  return checkoutUrl
-    .isValid(textObject)
-    .then((valid) => valid);
-};
-
-const isAdded = (text, data) => {
-  const added = data.filter((channel) => _.includes(channel, text));
-  return added.length > 0;
-};
-
-const validate = (text, data) => {
-  const errors = {};
-  return isValidUrl(text).then((valid) => {
-    if (!valid) {
-      errors.url = errorMessage.url;
-    }
-    if (isAdded(text, data)) {
-      errors.double = errorMessage.double;
-    }
-    return errors;
-  });
-};
+import validate from './validator';
 
 const updateValidationState = (state) => {
   validate(state.form.urlField, state.feed.channels)
@@ -58,12 +23,12 @@ const state = {
   },
   feed: {
     channels: [],
+    errors: {},
   },
 };
 
 const form = document.getElementById('form');
 const field = document.getElementById('url-address');
-const button = document.querySelector('#form button[type="submit"]');
 
 field.addEventListener('input', (e) => {
   state.form.urlField = e.target.value;
@@ -75,46 +40,32 @@ const proxy = 'cors-anywhere.herokuapp.com';
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   state.form.processState = 'fulfilled';
-  const channelData = {
-    name: state.form.urlField,
-  };
   const link = state.form.urlField;
   const requestUrl = `https://${proxy}/${link}`;
+
   axios.get(requestUrl)
     .then((response) => {
-      const feedData = parseRSS(response);
-      channelData.data = feedData;
+      const feedData = parseRSS(response.data);
+      if (_.isEqual(feedData, {})) {
+        state.feed.errors.data = 'There is no RSS data.';
+        state.form.processState = 'filling';
+      } else {
+        const channelData = {
+          name: link,
+          data: feedData,
+          id: _.uniqueId(),
+        };
+        state.feed.channels = [channelData, ...state.feed.channels];
+      }
+    })
+    .catch((error) => {
+      state.feed.errors.base = 'Network problems. Try again.';
+      state.form.processState = 'filling';
+      throw error;
+    }).then(() => {
+      state.form.urlField = '';
     });
-  state.feed.channels.push(channelData);
-  state.form.urlField = '';
   console.log(state);
 });
 
-watch(state.form, 'errors', () => {
-  const errorElement = field.nextElementSibling;
-  const errMessage = Object.values(state.form.errors).flat();
-  const value = state.form.urlField;
-  if (errorElement) {
-    field.classList.remove('is-invalid');
-    errorElement.remove();
-  }
-  if (!errMessage.length || !value.length) {
-    return;
-  }
-  const feedbackMessage = document.createElement('div');
-  feedbackMessage.classList.add('invalid-feedback');
-  feedbackMessage.innerHTML = errMessage;
-  field.classList.add('is-invalid');
-  field.after(feedbackMessage);
-});
-
-watch(state.form, 'valid', () => {
-  button.disabled = !state.form.valid;
-});
-
-watch(state.form, 'processState', () => {
-  if (state.form.processState === 'fulfilled') {
-    field.value = '';
-    button.disabled = true;
-  }
-});
+render(state);
