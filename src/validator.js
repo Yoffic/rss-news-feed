de88887e780b43b4f40bbd0407e27fd367bc15c7
Olga Ioffe <yoffic@yahoo.com>
2020-02-09 21:07:39 +0300

    Update: validator.js

--- src/validator.js
+++ src/validator.js
@@ -1,38 +1,33 @@
 import * as yup from 'yup';
-import { includes } from 'lodash';
 
 const errorMessage = {
   url: 'Invalid URL',
   repetition: 'This URL has already been added.',
 };
 
-const checkoutUrl = yup.object().shape({
-  urlAddress: yup.string().url().required(),
-});
+const checkoutUrl = yup.string().url().required();
 
-const isValidUrl = (text) => {
-  const textObject = { urlAddress: text };
-  return checkoutUrl
-    .isValid(textObject)
-    .then((valid) => valid);
+const isValid = (url) => (
+  checkoutUrl.isValid(url).then((valid) => valid)
+);
+
+const isAdded = (list, item) => {
+  const [result] = list.filter((elem) => elem.link === item);
+  return result !== undefined;
 };
 
-const isAdded = (text, data) => {
-  const added = data.filter((channel) => includes(channel, text));
-  return added.length > 0;
-};
-
-const validate = (text, data) => {
+const validate = (urlList, urlAddress) => {
   const errors = {};
-  return isValidUrl(text).then((valid) => {
-    if (!valid) {
-      errors.url = errorMessage.url;
-    }
-    if (isAdded(text, data)) {
-      errors.repetition = errorMessage.repetition;
-    }
-    return errors;
-  });
+  return isValid(urlAddress)
+    .then((valid) => {
+      if (!valid) {
+        errors.url = errorMessage.url;
+      }
+      if (isAdded(urlList, urlAddress)) {
+        errors.repetition = errorMessage.repetition;
+      }
+      return errors;
+    });
 };
 
 export default validate;
