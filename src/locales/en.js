4cd83b4dcece3209f746cdd96b1a7756460599d1
Olga Ioffe <yoffic@yahoo.com>
2020-02-10 13:16:16 +0300

    Add: locale resources for texts

---
+++ src/locales/en.js
@@ -0,0 +1,15 @@
+export default {
+  translation: {
+    errors: {
+      input: {
+        url: 'Invalid URL',
+        repetition: 'This URL has already been added.',
+      },
+      feed: {
+        data: 'Check the RSS channel. There is no RSS data for display.',
+        direction: 'URL not found. Please try again.',
+        base: 'Network problems. Please try again.',
+      },
+    },
+  },
+};
