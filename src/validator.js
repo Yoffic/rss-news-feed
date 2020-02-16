59b3a1870c56dbab4573100656302593bbf1986f
Olga Ioffe <yoffic@yahoo.com>
2020-02-16 11:33:33 +0300

    Remove: i18next initialization from validator.js

--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,4 @@
 import * as yup from 'yup';
-import i18next from 'i18next';
-import resources from './locales';
 
 const checkoutUrl = yup.string().url().required();
 
@@ -8,29 +6,23 @@ const isValid = (url) => (
   checkoutUrl.isValid(url).then((valid) => valid)
 );
 
-const isAdded = (list, item) => {
-  const [result] = list.filter((elem) => elem.link === item);
+const isAdded = (list, url) => {
+  const [result] = list.filter((elem) => elem.url === url);
   return result !== undefined;
 };
 
-const validate = (urlList, urlAddress) => (
-  i18next.init({
-    lng: 'en',
-    debug: false,
-    resources,
-  }).then((t) => {
-    const errors = {};
-    return isValid(urlAddress)
-      .then((valid) => {
-        if (!valid) {
-          errors.url = t('errors.input.url');
-        }
-        if (isAdded(urlList, urlAddress)) {
-          errors.repetition = t('errors.input.repetition');
-        }
-        return errors;
-      });
-  })
-);
+const validate = (urlList, urlAddress, texts) => {
+  const errors = {};
+  return isValid(urlAddress)
+    .then((valid) => {
+      if (!valid) {
+        errors.url = texts('errors.input.url');
+      }
+      if (isAdded(urlList, urlAddress)) {
+        errors.repetition = texts('errors.input.repetition');
+      }
+      return errors;
+    });
+};
 
 export default validate;
