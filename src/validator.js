cdaada65adb46915b878d2b8aebf2c084cb2ee87
Olga Ioffe <yoffic@yahoo.com>
2020-02-26 01:14:31 +0300

    Refactor: validator

--- src/validator.js
+++ src/validator.js
@@ -1,22 +1,9 @@
 import * as yup from 'yup';
-import { find } from 'lodash';
 
-const checkoutUrl = yup.string().url().required();
-
-const isValid = (url) => checkoutUrl.validateSync(url);
-
-const isAdded = (list, url) => {
-  const result = find(list, ['url', url]);
-  return !!result;
-};
-
-const validate = (urlList, urlAddress) => {
-  if (isAdded(urlList, urlAddress)) {
-    const error = new Error();
-    error.type = 'repetition';
-    throw error;
-  }
-  return isValid(urlAddress);
-};
-
-export default validate;
+export default (url, list) => (
+  yup.string()
+    .url()
+    .required()
+    .notOneOf(list)
+    .validateSync(url)
+);
